eaf30cd53ee36e0a6b8505dc2c061e6730d2bbf6
kimzing <kimzing@163.com>
2020-07-05 13:21:36 +0800

    完成controller代码生成

--- entry-app/src/main/java/com/kimzing/user/controller/user/UserController.java
+++ entry-app/src/main/java/com/kimzing/user/controller/user/UserController.java
@@ -1,16 +1,18 @@
 package com.kimzing.user.controller.user;
 
 
-import org.springframework.web.bind.annotation.*;
-import org.apache.dubbo.config.annotation.Reference;
-import io.swagger.annotations.ApiOperation;
-import com.kimzing.web.resolver.json.JsonParam;
+import com.kimzing.order.domain.OrderBO;
+import com.kimzing.order.service.OrderService;
+import com.kimzing.user.domain.user.*;
+import com.kimzing.user.service.user.UserService;
 import com.kimzing.utils.page.PageParam;
 import com.kimzing.utils.page.PageResult;
-import com.kimzing.user.service.user.UserService;
-import com.kimzing.user.domain.user.*;
+import com.kimzing.web.resolver.json.JsonParam;
+import io.swagger.annotations.ApiOperation;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * 用户信息接口层.
@@ -24,6 +26,9 @@ public class UserController {
 
     @Reference
     UserService userService;
+
+    @Reference
+    OrderService orderService;
 
     @ApiOperation(value = "保存用户信息")
     @PostMapping
@@ -53,6 +58,19 @@ public class UserController {
     @GetMapping("/list")
     public PageResult<UserBO> listPage(@JsonParam UserQueryDTO userQueryDTO, @ModelAttribute PageParam pageParam) {
         return userService.listPage(userQueryDTO, pageParam);
+    }
+
+    /**
+     * 查询用户信息及其订单信息(演示聚合的使用)
+     * @param id
+     * @param pageParam
+     * @return
+     */
+    @GetMapping("/user-order/{id}")
+    public UserOrderVO getUserAndOrderList(@PathVariable("id") Integer id, @ModelAttribute PageParam pageParam) {
+        UserBO userBO = userService.get(id);
+        List<OrderBO> orderBOList = orderService.listPageByUserId(id, pageParam);
+        return new UserOrderVO().setUserInfo(userBO).setOrderList(orderBOList);
     }
 
 }
