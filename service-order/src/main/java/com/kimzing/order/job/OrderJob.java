12aeb62132a43bade65be680e527b6f488c19ffe
kimzing <kimzing@163.com>
2020-07-17 02:39:44 +0800

    添加定时任务示例

--- service-order/src/main/java/com/kimzing/order/job/OrderJob.java
+++ service-order/src/main/java/com/kimzing/order/job/OrderJob.java
@@ -20,6 +20,14 @@ import java.time.temporal.ChronoUnit;
 /**
  * 订单相关定时任务.
  *
+ *
+ *  XxlJob开发示例（Bean模式）
+ *
+ *  开发步骤：
+ *  1、在Spring Bean实例中，开发Job方法，方式格式要求为 "public ReturnT<String> execute(String param)"
+ *  2、为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ *  3、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
+ *
  * @author KimZing - kimzing@163.com
  * @since 2020/7/17 02:00
  */
