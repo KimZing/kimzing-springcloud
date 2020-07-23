52d38d2edc7e875faa6ca41f6a95fb8880b842ae
kimzing <kimzing@163.com>
2020-07-24 03:10:17 +0800

    完成页面基本布局

--- front-admin/src/App.vue
+++ front-admin/src/App.vue
@@ -1,33 +1,33 @@
 <template>
-    <div id="app">
-        <HelloWorld/>
-    </div>
+  <div id="app">
+    <Main/>
+  </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+  import Main from './components/Main.vue'
 
-    export default {
-        name: 'App',
-        components: {
-            HelloWorld
-        }
+  export default {
+    name: 'App',
+    components: {
+      Main
     }
+  }
 </script>
 
 <style>
-    body {
-        /*去除与浏览器的空白边距*/
-        padding: 0px;
-        margin: 0px;
-        height: 100%;
-    }
+  body {
+    /*去除与浏览器的空白边距*/
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+  }
 
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        /*抗锯齿*/
-        -webkit-font-smoothing: antialiased;/*chrome、safari*/
-        -moz-osx-font-smoothing: grayscale;/*firefox*/
-    }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    /*抗锯齿*/
+    -webkit-font-smoothing: antialiased; /*chrome、safari*/
+    -moz-osx-font-smoothing: grayscale; /*firefox*/
+  }
 
 </style>
