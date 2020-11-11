7badf6b86e1c6e9e35926daa172338685b7f3634
kimzing <kimzing@163.com>
2020-11-11 22:06:42 +0800

    update

--- service-order/src/main/java/com/kimzing/order/publisher/OrderPublisher.java
+++ service-order/src/main/java/com/kimzing/order/publisher/OrderPublisher.java
@@ -2,6 +2,7 @@ package com.kimzing.order.publisher;
 
 import com.kimzing.order.domain.order.OrderCheckCancelEvent;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -23,17 +24,7 @@ public class OrderPublisher {
      * @param orderCheckCancelEvent
      */
     public void publishOrderCheckCancelEvent(OrderCheckCancelEvent orderCheckCancelEvent) {
-        // rocketMQTemplate.asyncSend(OrderCheckCancelEvent.TOPIC, MessageBuilder.withPayload(orderCheckCancelEvent).build(),
-        //         new SendCallback() {
-        //             @Override
-        //             public void onSuccess(SendResult sendResult) {
-        //                 LogUtil.info("消息[{}]发送成功: [{}]", orderCheckCancelEvent, sendResult);
-        //             }
-        //
-        //             @Override
-        //             public void onException(Throwable e) {
-        //                 LogUtil.error("消息[{}]发送失败: [{}]", orderCheckCancelEvent, e);
-        //             }
-        //         }, 3000, 15);
+        rocketMQTemplate.syncSend(OrderCheckCancelEvent.TOPIC, MessageBuilder.withPayload(orderCheckCancelEvent).build(),
+                3000, 15);
     }
 }
